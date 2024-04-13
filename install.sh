4437afc93167ccc52e5a04640e3993d25c25b8d0
Harist Dani Hisyam <23426661+HDHR@users.noreply.github.com>
2024-04-13 08:34:47 +0700

    Only replace system emoji

--- install.sh
+++ install.sh
@@ -5,41 +5,33 @@
 ##########################################################################################
 
 SKIPMOUNT=false
-
 PROPFILE=false
-
 POSTFSDATA=false
-
 LATESTARTSERVICE=false
-
 REPLACE_EXAMPLE="
 /system/app/Youtube
 /system/priv-app/SystemUI
 /system/priv-app/Settings
 /system/framework
 "
-
 REPLACE="
 "
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "*       iOS Emoji 16.4        *"
+  ui_print "*        FB Emoji 15.0        *"
   ui_print "*******************************"
 }
 
 on_install() {
-  #Definitions
-  MSG_DIR="/data/data/com.facebook.orca"
-  FB_DIR="/data/data/com.facebook.katana"
-  EMOJI_DIR="app_ras_blobs"
+  # Definitions
   FONT_DIR=$MODPATH/system/fonts
   FONT_EMOJI="NotoColorEmoji.ttf"
   ui_print "- Extracting module files"
   ui_print "- Installing Emojis"
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 
-  #Compatibility with different devices and potential Support for Android 13?
+  # Compatibility with different devices and potential Support for Android 13?
   variants='SamsungColorEmoji.ttf LGNotoColorEmoji.ttf HTC_ColorEmoji.ttf AndroidEmoji-htc.ttf ColorUniEmoji.ttf DcmColorEmoji.ttf CombinedColorEmoji.ttf NotoColorEmojiLegacy.ttf'
   for i in $variants ; do
         if [ -f "/system/fonts/$i" ]; then
@@ -47,29 +39,9 @@ on_install() {
         fi
   done
   
-  #Facebook Messenger
-  if [ -d "$MSG_DIR" ]; then
-    ui_print "- Replacing Messenger Emojis"
-    cd $MSG_DIR
-    rm -rf $EMOJI_DIR
-    mkdir $EMOJI_DIR
-    cd $EMOJI_DIR
-    cp $MODPATH/system/fonts/$FONT_EMOJI ./FacebookEmoji.ttf
-  fi
-  
-  #Facebook App
-  if [ -d "$FB_DIR" ]; then
-    ui_print "- Replacing Facebook Emojis"
-    cd $FB_DIR
-    rm -rf $EMOJI_DIR
-    mkdir $EMOJI_DIR
-    cd $EMOJI_DIR
-    cp $MODPATH/system/fonts/$FONT_EMOJI ./FacebookEmoji.ttf
-  fi
-  
-  #Verifying Android version
+  # Verifying Android version
   android_ver=$(getprop ro.build.version.sdk)
-  #if Android 12 detected
+  # If Android 12 detected
   if [ $android_ver -ge 31 ]; then
         DATA_FONT_DIR="/data/fonts/files"
     if [ -d "$DATA_FONT_DIR" ] && [ "$(ls -A $DATA_FONT_DIR)" ]; then
@@ -97,8 +69,5 @@ on_install() {
 }
 
 set_permissions() {
-  set_perm_recursive $MODPATH 0 0 0755 0644
-  set_perm_recursive /data/data/com.facebook.katana/app_ras_blobs/FacebookEmoji.ttf 0 0 0755 700
-  set_perm_recursive /data/data/com.facebook.katana/app_ras_blobs 0 0 0755 755
-  set_perm_recursive /data/data/com.facebook.orca/app_ras_blobs/FacebookEmoji.ttf 0 0 0755 700
+  set_perm_recursive $MODPATH 0 0 0755 06440
 }
